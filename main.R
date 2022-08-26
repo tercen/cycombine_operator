236f22544609a016df9cf8b41e159c965a18239c
lucas501 <lucas.lelann@gmail.com>
2022-08-26 14:09:58 +0000

    update main.R and json files

--- main.R
+++ main.R
@@ -4,33 +4,46 @@ library(cyCombine)
 library(magrittr)
 library(tidyverse)
 
-ctx = tercenCtx()
+ctx <- tercenCtx()
 
 data.all<-ctx$select(unlist(list(".y",".ci",".ri",ctx$colors,ctx$labels)))
-
 #data.all<-data.all[c(0:10000),]
-
 data <- data.all[0:3] %>% 
   pivot_wider(id_cols=".ci",names_from= ".ri", values_from =".y")
 
-colnames(data) <- c(".ri",ctx$rselect()[[1]])
-markers<-colnames(data)
+colnames(data) <- c(".ci",ctx$rselect()[[1]])
+markers<-ctx$rselect()[[1]]
 
-uncorrected.all<-merge(x=data,y=data.all[,3:ncol(data.all)],by = ".ri")
+uncorrected.all<-dplyr::full_join(x=data,y=data.all[,2:ncol(data.all)],by = ".ci")
+
+#uncorrected.all<-merge(x=data,y=data.all[,3:ncol(data.all)],by = ".ri")
 
 colnames(uncorrected.all)[grep(pattern = "atch",x = colnames(uncorrected.all))]<-"batch"
 colnames(uncorrected.all)[grep(pattern = "ondition",x = colnames(uncorrected.all))]<-"condition"
 
-# Run batch correction
-labels <- uncorrected.all %>%
-  normalize(markers = markers,
-            norm_method = "scale") %>%
-  create_som(markers = markers,
-             rlen = 10)
+#uncorrected.all<-uncorrected.all %>% drop_na(batch)
+uncorrected.all<-uncorrected.all %>% drop_na()
 
-corrected <- uncorrected.all %>%
+uncorrected<-select(uncorrected.all, -.ri)
+uncorrected %<>% mutate(batch = as.integer(batch))
+# Run batch correction
+labels.ori <- uncorrected %>%
+  normalize(markers = markers,
+            norm_method = "rank")  #scale or rank
+
+labels<-labels.ori %>%
+  create_som(markers = markers,
+             rlen = 10,#Higher values are recommended if 10 does not appear to perform well
+             xdim = 8,
+             ydim = 8) 
+
+corrected <- uncorrected %>%
   correct_data(label = labels,
-               covar = "condition")
+               markers = markers,
+               anchor = NULL,
+               covar = "condition",
+               parametric = TRUE)
+
 corrected%>%
   ctx$addNamespace() %>%
   ctx$save()
